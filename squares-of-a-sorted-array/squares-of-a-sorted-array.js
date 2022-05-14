40d4524dec9a1cf55bc5d078705842d84cc62ca2
Luna <91652033+Luna-Runa@users.noreply.github.com>
2022-05-14 21:59:25 +0900

    Time: 165 ms, Memory: 49.2 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,24 +3,8 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let i = 0;
-    let nd = false;
-   
-    if(nums.length < 2)
-        return nums.map(e => e *= e)
     
-    while(i < nums.length) {
-        if(nums[i] <= nums[i+1]) {
-            nd = true;
-            break;
-        }
-        i++;
-    }
+        return answer = nums.map(e => e *= e).sort((a, b) => a - b);
 
-    if(nd === true) {
-        let answer = nums.map(e => e *= e)
-        answer.sort((a, b) => a - b);
-        return answer;
-    }
     
 };
