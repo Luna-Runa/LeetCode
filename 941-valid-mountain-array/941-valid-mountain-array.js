701b3c2aae2263fd32c099b8563bb8eaf9992aa1
Luna <91652033+Luna-Runa@users.noreply.github.com>
2022-05-16 08:00:18 +0900

    Time: 117 ms (16.44%), Space: 44 MB (98.26%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function(arr) {
+    if(arr.length < 3)
+        return false;
+    
+    let prev = -1;
+    let ud = 1;
+    
+    for(let i = 0; i < arr.length; i++) {
+        //start but down
+        if(arr[0] > arr[1]) return false;
+        
+        //req up but down
+        else if(ud === 1 && prev > arr[i]) ud = -1;
+        
+        //req down but up
+        else if(ud === -1 && prev < arr[i]) return false;
+        
+        //straight
+        else if(prev === arr[i]) return false;
+    
+        //req up arr up && req down arr down && not straight
+        prev = arr[i];
+    }
+    
+    //ud not reverse = false
+    return ud === -1? true: false;
+    
+};
