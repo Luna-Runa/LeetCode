d5603241d54aeaf916cf5b60334197955f5769fd
Luna <91652033+Luna-Runa@users.noreply.github.com>
2022-05-28 23:45:42 +0900

    Time: 112 ms, Memory: 42.1 MB - LeetHub

--- height-checker/height-checker.js
+++ height-checker/height-checker.js
@@ -4,7 +4,7 @@
  */
 const heightChecker = function(heights) {
     let k = 0;
-    origin = Object.assign([], heights)
+    origin = [...heights]
     heights.sort((a, b) => a - b)
     
     heights.forEach((e, i) => {
