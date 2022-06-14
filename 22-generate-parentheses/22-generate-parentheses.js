bf3caeb686f66c7fa510439e9d39d8e58c6e897e
Luna <eternality623@gmail.com>
2022-06-14 19:57:31 +0900

    Time: 62 ms (93.84%), Space: 42.7 MB (64.58%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const answer = [];
+    
+    function dfs(text, l, r) {
+        if(text.length === 2 * n) {
+            answer.push(text);
+            return;
+        }
+        
+        if(l < n) dfs(text+"(", l+1, r)
+        
+        if(r < l) dfs(text+")", l, r+1)
+    }
+    
+    dfs("", 0, 0)
+    
+    return answer; 
+};
